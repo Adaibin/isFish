26531f0d3334100624eaa1e8607572ffae0f670c
adaibin <yybzjn@163.com>
2018-03-09 11:20:19 +0800

    add

--- dairy/creator.py
+++ dairy/creator.py
@@ -19,7 +19,8 @@ def folder_():
         folders_ = f_.readlines()
         folders_new = [f for f in folders if f not in folders_]
     with open('folder.fish', 'w') as f_:
-        f_.writelines(folders_new)
+        for line in folders:
+            f_.write(line + '\n')
 
     return folders_new
 
@@ -30,7 +31,7 @@ def creator_(folder):
     path_ = '/'.join((os.getcwd(), folder))
     content_file = '/'.join((path_, 'content.fish'))
     if not os.path.exists(content_file):
-        print('there is no content file in this folder.')
+        print('there is no content file in %s.' % folder)
         return False
     with open(content_file, 'r') as f_:
         content_ = f_.readlines()
@@ -47,20 +48,25 @@ def creator_(folder):
     images = files_['.jpg'] + files_['.png']
     musics = files_['.mp3'] + files_['.wma']
     images.sort(), musics.sort()
+    # remove qr code file
+    if title + '.png' in images:
+        images.remove(title + '.png')
+    music = musics[0] if musics else musics
 
     # render page
     with open(os.getcwd() + '/template.html', 'r', encoding='utf-8') as f:
         template = Template(f.read())
         text = {'title': title,
+                'folder': folder,
                 'context': context,
                 'images': images,
-                'musics': musics,
+                'music': music,
                 'date': datetime.now()}
         html = template.render(text=text)
-
     with open(path_ + '/' + title + '.html', 'w', encoding='utf-8') as wf:
         wf.write(html)
 
+    # create qr code image
     qr = qrcode.QRCode(version=7,
                        error_correction=qrcode.constants.ERROR_CORRECT_M,
                        box_size=10,
@@ -68,10 +74,9 @@ def creator_(folder):
 
     qr.add_data('%s/dairy/%s/%s.html' % (domain, folder, title))
     qr.make()
-
     img = qr.make_image(fill_color="white", back_color="blue")
     img.save(open('%s/%s.png' % (folder, title), 'wb'))
 
 
-creator_('20180309')
-
+for folder in folder_():
+    creator_(folder)
