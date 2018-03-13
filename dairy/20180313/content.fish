c3d632c0259ed0c146d039a427b63b5d381724a0
adaibin <yybzjn@163.com>
2018-03-13 14:50:17 +0800

    add

---
+++ dairy/20180313/content.fish
@@ -0,0 +1,17 @@
+Mitsuha
+Once in a while when I wake up
+I find myself crying
+The dream I must have had
+I can never recall
+But...But...
+The sensation that I've lost something
+lingers for a long time after I wake up
+I am always searching
+for something, for someone
+This feeling has possessed me
+I think from that day...
+The day when the stars come falling
+It was almost as if...
+as if a scene from a dream
+Nothing more, Nothing less
+than a beautiful view.
