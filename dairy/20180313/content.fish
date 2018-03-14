b2a72ace417626d1f40646717f98d580ded74a47
adaibin <yybzjn@163.com>
2018-03-14 15:37:00 +0800

    add

--- dairy/20180313/content.fish
+++ dairy/20180313/content.fish
@@ -10,8 +10,146 @@ I am always searching
 for something, for someone
 This feeling has possessed me
 I think from that day...
-The day when the stars come falling
+The day when the stars came falling
 It was almost as if...
 as if a scene from a dream
 Nothing more, Nothing less
 than a beautiful view.
+
+
+
+Mitsuha, What are you doing?
+Well, It feels incredibly realistic...
+What? "Mitsuha"?
+Are you half asleep? Breakfast!
+Hurry up!
+What? What?
+Do you want last night's leftovers?
+You can have it.
+Good morning.
+Mitsuha, you are late!
+I will make breakfast tomorrow.
+Is this too much?
+Oh, well.
+You are normal today.
+You were nuts yesterday.
+What are you talking about?
+Good morining, everyone.
+This is an announcement from Itomori Town Hall.
+Regarding the mayoral election,
+to be held on the 20th next month, the election committee...
+A comet with an orbital period of 1200 years will pass by in a month.
+The comet will be visible to the naked eys for several days...
+Just make it up with him.
+It is an adult's matter.
+"Position of Comet Timat"
+JAXA are preparing to observe the celestial show of the century.
+"Closest on October 4th."
+We're going now!
+Study hard!
+Mitsuha
+Morning, Sayaka, Tessie.
+Morning
+Get off, Why? Meanie.
+You're heavy.
+That is rude.
+You two get along so well.
+We do not!
+Your hair looks all right today.
+
+
+
+
+I'd rather do that too.
+You're not ready yet, Yotsuha.
+Listen to the thread's voice.
+When you keep twining like that,
+emotions will eventually start flowing between you and the thread.
+Threads don't talk.
+She means "concentrate."
+1000 years of Itomori's history is etched into our braided cords.
+Listen, 200 years ago...
+Here she goes again.
+Sandal make Mayugoro's bathroom caught on fire and burned down this whole area.
+The shrine and old documents were destroyed.
+and this is known as...
+The Great Fire of Mayugoro.
+The fire has his name? Poor Mayugoro.
+So the meaning of our festivals became unknown and only the form lived on.
+But even if words was lost, tradition should be handed down.
+That's the important task we at Miyamizu Shrine have.
+But that foolish son-in-law...
+As if leaving Shinto priesthood and this house weren't enough,
+now politics? He is hopeless.
+Have another, my man.
+I'm counting on your help again.
+Leave it to me.
+You'll get votes from the Kadoiri and Sakagami Districts.
+How's it going with her?
+It's not that easy.
+I smell corruption.
+Dot't be ridiculous.
+Bring us a few more hot sake.
+Ok.
+Katsuhiko, work on site this weekend.
+Learn how to use explosives.
+Can't hear you.
+Yes!
+We both have it rough, don't we?
+Is that Yotsuha? She's all grown up.
+They're both pretty like their mother.
+Hey.
+Hey.
+It's the oldest sake in the world.
+By chewing on rice, spitting it out, and letting it ferment, it becomes alcohol.
+"Kuchikamisake." Do the gods appreciate sake made that way?
+Of course they do.
+Hey, look, It's Miyamizu.
+Oh! I could never do that!
+Isn't she embarrassed?
+I can't believe it.
+Cheer up, Mitsuha, Who cares if a few classmates saw you?
+I envy you pre-pubescent lack of concern.
+Why not sell lots of Kuchikamisake and use the money to go to Tokyo?
+Where do you get such ideas?
+Sell them with photos and making videos, Name it "Shrine Maiden's Sake"
+You will make money!
+No, That breaks the Liguor Tax Law.
+That is the problem?
+I hate this town! I hate this life!
+Please make me a handsome Tokyo boy in my next lift!
+What a fool...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
