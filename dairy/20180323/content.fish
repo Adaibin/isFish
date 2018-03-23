1252a76d6935190b671e89e419cb8ef719979462
adaibin <yybzjn@163.com>
2018-03-23 16:59:03 +0800

    fix

--- dairy/20180323/content.fish
+++ dairy/20180323/content.fish
@@ -1,18 +1,12 @@
 AndNextOne
-Understand,
-Trust,
-Support,
-you.
+Understand, Trust, Support you.
 For
-current
-life.
-And the next one.
+current life!
+And the next one!
 
 
 
-理解、
-信任、
-支持、
+理解 信任 支持,
 你,
 这辈子,
 还有来生.
